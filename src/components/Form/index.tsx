f59407c150a7ce37941b22aa030764c2d0ce3a00
FUJIAWESOME <dkorotchikov123@gmail.com>
2024-03-21 00:14:53 +0300

    refactoring

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,63 +1,164 @@
 import React, { useState } from "react";
 import Select from "../Select";
 import {
-  COUNTRIES,
-  accommodationTypesForRB,
-  accommodationTypesForRF,
+  COUNTRIE,
+  SELECTOR_TYPE,
+  accommodationOptionsForRussia,
+  accommodationOptionsForBelarus,
   countriesOptions,
-  countryCities,
-  universityTypes,
+  universityOptions,
+  citiesFromRussia,
+  citiesFromBelarus,
+  universityOptionsSochi,
+  CITY,
+  UNIVERSITY_TYPE,
+  technolohyFacultyOptions,
+  humantiesFacultyOptions,
+  SEQUENCE_NUMBER,
 } from "../../consts";
-import { IFormData } from "../../interfaces";
 import Button from "../Button";
 import styles from "./Form.module.css";
+import { IFormData, IFormValue } from "../../interfaces";
 
-const initialState = {
-  country: "",
-  city: "",
-  universityType: "",
-  accommodationType: "",
-};
+const initialState = [
+  {
+    sequenceNumber: SEQUENCE_NUMBER.FIRST,
+    selectorName: SELECTOR_TYPE.COUNTRY,
+    value: "",
+    isVisibleNext: false,
+  },
+  {
+    sequenceNumber: SEQUENCE_NUMBER.SECOND,
+    selectorName: SELECTOR_TYPE.CITY,
+    value: "",
+    isVisibleNext: false,
+  },
+  {
+    sequenceNumber: SEQUENCE_NUMBER.THIRD,
+    selectorName: SELECTOR_TYPE.UNIVERSITY_TYPE,
+    value: "",
+    isVisibleNext: false,
+  },
+  {
+    sequenceNumber: SEQUENCE_NUMBER.FOURTH,
+    selectorName: SELECTOR_TYPE.ACCOMMODATION_TYPE,
+    value: "",
+    isVisibleNext: false,
+  },
+  {
+    sequenceNumber: SEQUENCE_NUMBER.FIFTH,
+    selectorName: SELECTOR_TYPE.FACULTY,
+    value: "",
+    isVisibleNext: false,
+  },
+];
 
 const Form: React.FC = () => {
-  const [formData, setFormData] = useState<IFormData>(initialState);
+  const [formData, setFormData] = useState<Array<IFormData>>(initialState);
 
-  const accommodationTypes =
-    formData.country === COUNTRIES.RF
-      ? accommodationTypesForRF
-      : accommodationTypesForRB;
+  const formValues = formData.reduce<IFormValue>((acc, item: IFormData) => {
+    acc[item.selectorName] = {
+      value: item.value,
+      isVisibleNext: item.isVisibleNext,
+    };
+    return acc;
+  }, {});
 
-  const handleCountryChange = (country: string) => {
-    setFormData({
-      ...initialState,
-      country,
+  const accommodationOptions =
+    formValues.country?.value === COUNTRIE.RUSSIAN_FEDERADITON
+      ? accommodationOptionsForRussia
+      : accommodationOptionsForBelarus;
+
+  const citiesOptions =
+    formValues.country?.value === COUNTRIE.RUSSIAN_FEDERADITON
+      ? citiesFromRussia
+      : citiesFromBelarus;
+
+  const currentUniversityOptions =
+    formValues.city?.value === CITY.SOCHI
+      ? universityOptionsSochi
+      : universityOptions;
+
+  const facultyOptions =
+    formValues.universityType?.value === UNIVERSITY_TYPE.TECHNICAL
+      ? technolohyFacultyOptions
+      : humantiesFacultyOptions;
+
+  const getNewFormData = (
+    selectorName: string,
+    newValue: string,
+    sequenceNumber: number
+  ) => {
+    const newFormData = formData.map((item) => {
+      const currentSequenceNumber = item.sequenceNumber;
+      const currentSelectorName = item.selectorName;
+      if (currentSelectorName === selectorName) {
+        return {
+          ...item,
+          value: newValue,
+          isVisibleNext: !!newValue,
+        };
+      } else if (currentSequenceNumber > sequenceNumber) {
+        return {
+          ...item,
+          value: "",
+          isVisibleNext: false,
+        };
+      } else {
+        return item;
+      }
     });
+
+    return newFormData;
   };
 
-  const handleCityChange = (city: string) => {
-    setFormData({
-      ...initialState,
-      country: formData.country,
-      city,
-    });
+  const handleCountryChange = (value: string) => {
+    const newFormData = getNewFormData(
+      SELECTOR_TYPE.COUNTRY,
+      value,
+      SEQUENCE_NUMBER.FIRST
+    );
+    setFormData(newFormData);
   };
 
-  const handleUniversityTypeChange = (universityType: string) => {
-    setFormData({
-      ...formData,
-      universityType,
-    });
+  const handleCityChange = (value: string) => {
+    const newFormData = getNewFormData(
+      SELECTOR_TYPE.CITY,
+      value,
+      SEQUENCE_NUMBER.SECOND
+    );
+    setFormData(newFormData);
   };
 
-  const handleAccommodationTypeChange = (accommodationType: string) => {
-    setFormData({
-      ...formData,
-      accommodationType,
-    });
+  const handleUniversityTypeChange = (value: string) => {
+    const newFormData = getNewFormData(
+      SELECTOR_TYPE.UNIVERSITY_TYPE,
+      value,
+      SEQUENCE_NUMBER.THIRD
+    );
+    setFormData(newFormData);
   };
 
-  const isSubmitDisabled = Object.values(formData).some(
-    (value) => value === ""
+  const handleAccommodationTypeChange = (value: string) => {
+    const newFormData = getNewFormData(
+      SELECTOR_TYPE.ACCOMMODATION_TYPE,
+      value,
+      SEQUENCE_NUMBER.FOURTH
+    );
+    setFormData(newFormData);
+  };
+
+  const handleFacultyTypeChange = (value: string) => {
+    const newFormData = getNewFormData(
+      SELECTOR_TYPE.FACULTY,
+      value,
+      SEQUENCE_NUMBER.FIFTH
+    );
+    setFormData(newFormData);
+  };
+
+  const isSubmitDisabled = Object.values(formValues).some(
+    (item) => item.value === ""
   );
 
   return (
@@ -66,31 +167,39 @@ const Form: React.FC = () => {
         title="Выберите страну"
         onChange={handleCountryChange}
         options={countriesOptions}
-        value={formData.country}
+        value={formValues.country?.value}
       />
 
       <Select
         title="Выберите город"
         onChange={handleCityChange}
-        isDisabled={!formData.country}
-        options={countryCities[formData.country]}
-        value={formData.city}
+        isDisabled={!formValues.country?.isVisibleNext}
+        options={citiesOptions}
+        value={formValues.city?.value}
       />
 
       <Select
         title="Выберите вид ВУЗа"
-        isDisabled={!formData.city}
+        isDisabled={!formValues.city?.isVisibleNext}
         onChange={handleUniversityTypeChange}
-        options={universityTypes}
-        value={formData.universityType}
+        options={currentUniversityOptions}
+        value={formValues.universityType?.value}
       />
 
       <Select
         title="Выберите вариант проживания"
         onChange={handleAccommodationTypeChange}
-        isDisabled={!formData.universityType}
-        options={accommodationTypes}
-        value={formData.accommodationType}
+        isDisabled={!formValues.universityType?.isVisibleNext}
+        options={accommodationOptions}
+        value={formValues.accommodationType?.value}
+      />
+
+      <Select
+        title="Выберите факультет"
+        onChange={handleFacultyTypeChange}
+        isDisabled={!formValues.accommodationType?.isVisibleNext}
+        options={facultyOptions}
+        value={formValues.faculty?.value}
       />
 
       <Button title="Отправить" isDisabled={isSubmitDisabled} />
